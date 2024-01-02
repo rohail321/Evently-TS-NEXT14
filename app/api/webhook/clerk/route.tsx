9bc664fda103caffc6a6ccc9e196d36ecdb236f7
rohail321 <qrohail32@gmail.com>
2024-01-02 05:23:28 +0000

    initial deploy

--- app/api/webhook/clerk/route.tsx
+++ app/api/webhook/clerk/route.tsx
@@ -1,7 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createUser } from "@/lib/actions/user.actions";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 import { clerkClient } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
@@ -94,17 +94,17 @@ export async function POST(req: Request) {
       photo: image_url,
     };
 
-    // const updatedUser = await updateUser(id, user)
+    const updatedUser = await updateUser(id, user);
 
-    // return NextResponse.json({ message: 'OK', user: updatedUser })
+    return NextResponse.json({ message: "OK", user: updatedUser });
   }
 
   if (eventType === "user.deleted") {
     const { id } = evt.data;
 
-    // const deletedUser = await deleteUser(id!)
+    const deletedUser = await deleteUser(id!);
 
-    // return NextResponse.json({ message: 'OK', user: deletedUser })
+    return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
   return new Response("", { status: 200 });
